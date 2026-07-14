4f7ce457e50522f4c7c2cc4318d74d16e1738e02
balajinatarajan21 <46074342+balajinatarajan21@users.noreply.github.com>
2026-07-14 11:27:06 +0530

    SEPolicy fixes

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,7 +82,6 @@ BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := \
     androidboot.hardware=qcom \
-    printk.devkmsg=on ignore_loglevel loglevel=7 \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
     cgroup.memory=nokmem,nosocket \
@@ -92,6 +91,7 @@ BOARD_KERNEL_CMDLINE := \
     reboot=panic_warm \
     service_locator.enable=1 \
     swiotlb=2048
+#     printk.devkmsg=on ignore_loglevel loglevel=7
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
